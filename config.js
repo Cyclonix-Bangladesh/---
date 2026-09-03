c0cf3d6207542dda9e3559cf8797e3916d29f066
Cyclonix-Bangladesh <cyclonix.bangladesh@gmail.com>
2026-09-04 00:26:01 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Supabase dashboard থেকে Project URL এবং anon key এখানে বসান
-window.SUPABASE_URL = 'https://vtqarznrvyvcrlotubzf.supabase.co/rest/v1/';
+window.SUPABASE_URL = 'vtqarznrvyvcrlotubzf';
 window.SUPABASE_ANON_KEY = 'sb_publishable_f6B9VBMscN5qIKKvDqpBTQ_jCtaJB1S';
