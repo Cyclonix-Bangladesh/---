11c09c38f6082d29c6fc17c3789ecff6dd966069
Cyclonix-Bangladesh <cyclonix.bangladesh@gmail.com>
2026-09-04 01:24:42 +0600

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Supabase dashboard থেকে Project URL এবং anon key এখানে বসান
-window.SUPABASE_URL = 'vtqarznrvyvcrlotubzf';
-window.SUPABASE_ANON_KEY = 'sb_publishable_f6B9VBMscN5qIKKvDqpBTQ_jCtaJB1S';
