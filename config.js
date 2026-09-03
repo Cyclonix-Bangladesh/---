c0bc5c2126a5ebbfa204b2fc049a0ea94c8224d6
Cyclonix-Bangladesh <cyclonix.bangladesh@gmail.com>
2026-09-04 03:31:48 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Supabase project configuration
 // Safe to use in frontend: Publishable/Anon key only.
-window.SUPABASE_URL = 'https://vtqarznrvyvcrlotubzf.supabase.co';
-window.SUPABASE_ANON_KEY = 'sb_publishable_f6B9VBMscN5qIKKvDqpBTQ_jCtaJB1S';
+window.SUPABASE_URL = 'https://btcxegaepzneeczwoofg.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_b8oi1SKRPrDgSCqUKfHl8A_Xay9OGPx';
